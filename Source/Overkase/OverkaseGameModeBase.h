3c1a51424762c79ea0e6df00b269e566973ea616
BladEO <amzi1118@gmail.com>
2023-08-01 18:03:10 +0900

    Default project

---
+++ Source/Overkase/OverkaseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OverkaseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERKASE_API AOverkaseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
