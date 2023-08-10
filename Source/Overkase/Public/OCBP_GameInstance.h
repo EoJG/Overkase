d743e2177334f5e7fcee74f8bcd14e190d4dc56b
hotstar89 <hotstar89@naver.com>
2023-08-10 15:25:25 +0900

    UI 코드작업 수정

---
+++ Source/Overkase/Public/OCBP_GameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "OCBP_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERKASE_API UOCBP_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+
+public:
+	UOCBP_GameInstance();
+};
