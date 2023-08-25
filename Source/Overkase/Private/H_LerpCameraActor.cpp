4fbe6deca25d20e8fffde03c9afc6e36de8e4292
githeed <tmdgmldhk11@gmail.com>
2023-08-25 13:19:32 +0900

    CameraMovingLerp

--- Source/Overkase/Private/H_LerpCameraActor.cpp
+++ Source/Overkase/Private/H_LerpCameraActor.cpp
@@ -7,6 +7,7 @@
 #include <GameFramework/SpringArmComponent.h>
 #include <Components/StaticMeshComponent.h>
 #include <Camera/CameraComponent.h>
+#include <Engine/Engine.h>
 
 // Sets default values
 AH_LerpCameraActor::AH_LerpCameraActor()
@@ -19,31 +20,78 @@ AH_LerpCameraActor::AH_LerpCameraActor()
 
 	springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
 	springArm->SetupAttachment(meshComp);
-	springArm->TargetArmLength = 1600;
+	springArm->TargetArmLength = targetArmLength;
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Cam"));
 	CameraComp->SetupAttachment(springArm);
 
+	springArm->SetIsReplicated(true);
 }
 
 // Called when the game starts or when spawned
 void AH_LerpCameraActor::BeginPlay()
 {
 	Super::BeginPlay();
-
+	if (springArm != nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Black, FString::Printf(TEXT("%s"), *springArm->GetName()));
 	
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Black, FString::Printf(TEXT("nulll")));
+	
+	}
 }
 
 // Called every frame
 void AH_LerpCameraActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-    
+
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AH_OverkaseCharacter::StaticClass(), PlayerActors);
 
 	FVector avgLoc = UGameplayStatics::GetActorArrayAverageLocation(PlayerActors);
 
-	FVector lerpLoc = FMath::Lerp(GetActorLocation(), avgLoc, 0.1f);
+	FVector lerpLoc = FMath::Lerp(GetActorLocation(), avgLoc, 0.01f);
 
-	SetActorLocation(lerpLoc);
+	if (avgLoc.X < -720.0f && avgLoc.X > -1200.0f && avgLoc.Y < 420.0f && avgLoc.Y > -100.0f)
+	{
+		SetActorLocation(lerpLoc);
+	}
+
+
+	if (PlayerActors.Num() >= 2)
+	{
+		float dist = FVector::Dist(PlayerActors[0]->GetActorLocation(), PlayerActors[1]->GetActorLocation());
+		GEngine->AddOnScreenDebugMessage(-1, 0.01f, FColor::Green, FString::Printf(TEXT("%f"), dist));
+		if (dist >= 900)
+		{
+
+			if (springArm != nullptr)
+			{
+				if (targetZoomOutLength < 1600)
+				{
+					float lerpFloat = FMath::Lerp(4, 1, lerpAlpha);
+					targetZoomOutLength = targetArmLength += lerpFloat;
+					springArm->TargetArmLength = targetZoomOutLength;
+					GEngine->AddOnScreenDebugMessage(-1, 0.01f, FColor::Black, FString::Printf(TEXT("%f"), targetZoomOutLength));
+
+				}
+			}
+			else { UE_LOG(LogTemp, Warning, TEXT("Nullllllll")); }
+		}
+		else
+		{
+			if (springArm != nullptr)
+			{
+				if (targetZoomOutLength > 1200)
+				{
+					float lerpFloat2 = FMath::Lerp(4, 1, lerpAlpha);
+					targetZoomOutLength = targetArmLength -= lerpFloat2;
+					springArm->TargetArmLength = targetZoomOutLength;
+				}
+			}
+		}
+	}
 }
--- Source/Overkase/Public/H_LerpCameraActor.h
+++ Source/Overkase/Public/H_LerpCameraActor.h
@@ -23,14 +23,21 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class UStaticMeshComponent* meshComp;
-	UPROPERTY(EditAnywhere)
-	class USpringArmComponent* springArm;
-	UPROPERTY(EditAnywhere)
-	class UCameraComponent* CameraComp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class USpringArmComponent* springArm;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UCameraComponent* CameraComp;
 
 	TArray<AActor*> PlayerActors;
 	TArray<class AH_OverkaseCharacter> overPlayerActors;
+
+	UPROPERTY(EditAnywhere, Category = CameraMoving)
+	float targetArmLength = 1400;
+	UPROPERTY(EditAnywhere, Category = CameraMoving)
+	float targetZoomOutLength = 0;
+	UPROPERTY(EditAnywhere, Category = CameraMoving)
+	float lerpAlpha = 0.5f;
 
 };
